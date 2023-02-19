527ec339f6ff712d295a19e47f839ed167e7147a
LU <101085637+LL-IU@users.noreply.github.com>
2023-02-19 16:26:35 +0800

    Add files via upload

--- mytab.js
+++ mytab.js
@@ -53,12 +53,31 @@ function myClick() {
     searchMy();
 }
 
+var keyArray = [81, 87, 69, 82, 84, 89, 85, 73, 79, 80, 65, 83, 68, 70, 71, 72, 74, 75, 76, 90, 88, 67, 86, 66, 78, 77];//按键盘排布的按键代码
+//在右键keytype后会停留在fn里，所以需要在那里也加入以下的按键事件，并添加一定的条件
 window.onkeydown = function (e) {
-    if (e.keyCode === 13) {
-        //enter不会让input丢失聚焦，所以要先取值，因为不能执行到remove里的取值
-        content = sbtn.value;
-        searchMy();
-    };
+    for (let i = 0; i < keytype.length; i++) {
+        if (sbtn != document.activeElement) {
+            if (e.keyCode === keyArray[i]) {//按下对应按键并且搜索框没有焦点
+                keytype[i].click();//点击对应i的链接
+            }
+        }
+    }
+    if (sbtn == document.activeElement) {//搜索框有焦点
+        if (e.keyCode == 27) {
+            sbtn.blur();//去焦点
+        }
+        if (e.keyCode == 13) {//enter进行搜索
+            //enter不会让input丢失聚焦，所以要先取值，因为不能执行到remove里的取值
+            content = sbtn.value;
+            searchMy();
+        };
+    } else {
+        if (e.keyCode == 27) {
+            sbtn.focus();//没有焦点时获得焦点
+        }
+    }
+
 }
 //quicklink部分，右键菜单和自定义网址
 var ql = document.getElementById("quickLink");//快捷链接的整体
@@ -86,34 +105,75 @@ for (let i = 0; i < keytype.length; i++) {
     function fn(e) {
         e.preventDefault();//preventDefault()阻止默认事件（这里阻止了默认菜单）
         myul.style.display = 'block';//点击右键菜单显示出来
-        let X = e.screenX;// - this.offsetLeft 
-        let Y = e.screenY - 60;//  - this.offsetTop 
+        let X = e.pageX;
+        let Y = e.pageY;
         myul.style.left = X + 'px';
         myul.style.top = Y + 'px';
+        linkInput.focus();
         //kt.classList.add("test");
         //enter后把输入的网址填入a标签的href中，让它能被访问，获取图标
+        //这里右键之后，只能监听到以下的onkeydown，需要把前面对搜索框的操作也添加到这里
         window.onkeydown = function (e) {
-            if (e.keyCode === 13) {
-                var newLink = linkInput.value;//获得输入的网址
-                var newIcon = iconInput.value;
-                kt.href = "https://" + newLink;//为什么要加"https://"，
-                //因为不定义href时会自带网页本身的链接，不加就会把输入内容直接填到本身的网址后面，加上后可以替换掉原本的href
-                var imgs = kt.getElementsByTagName('img').length;//用于判断是否存在img
-                if (imgs == 0) {
-                    //没有img时创建一个，放入网址图标
-                    var linkImg = document.createElement("img");
-                    linkImg.src = "https://" + newIcon;
-                    kt.appendChild(linkImg);
-                } else {
-                    //有img时替换src
-                    kt.querySelector("img").src = "https://" + newIcon;//querySelector可以获得img
+            if (myul.style.display == 'block') {
+                //var e = window.event || event;
+                if (e.keyCode == 40) {//左37，上38，右39，下40
+                    linkInput.blur();
+                    iconInput.focus();
                 }
-                myul.style.display = 'none';//输入完消失
-                linkInput.value = "";//输入框清空
-                iconInput.value = "";
-                //把数据存到localStorage
-                localStorage.setItem(i, "https://" + newLink);
-                localStorage.setItem(i + 27, "https://" + newIcon);
+                if (e.keyCode == 38) {
+                    iconInput.blur();
+                    linkInput.focus();
+                }
+                if (e.keyCode == 27) {//esc
+                    myul.style.display = 'none';
+                }
+                if (e.keyCode == 13) {//enter
+                    var newLink = linkInput.value;//获得输入的网址
+                    var newIcon = iconInput.value;
+                    kt.href = "https://" + newLink;//为什么要加"https://"，
+                    //因为不定义href时会自带网页本身的链接，不加就会把输入内容直接填到本身的网址后面，加上后可以替换掉原本的href
+                    var imgs = kt.getElementsByTagName('img').length;//用于判断是否存在img
+                    if (imgs == 0) {
+                        //没有img时创建一个，放入网址图标
+                        var linkImg = document.createElement("img");
+                        linkImg.src = "https://" + newIcon;
+                        kt.appendChild(linkImg);
+                    } else {
+                        //有img时替换src
+                        kt.querySelector("img").src = "https://" + newIcon;//querySelector可以获得img
+                    }
+                    myul.style.display = 'none';//输入完消失
+                    linkInput.value = "";//输入框清空
+                    iconInput.value = "";
+                    //把数据存到localStorage
+                    if (newLink || newIcon) {
+                        localStorage.setItem(i, "https://" + newLink);
+                        localStorage.setItem(i + 27, "https://" + newIcon);
+                    }
+                }
+            }
+            if (myul.style.display == 'none') {
+                for (let i = 0; i < keytype.length; i++) {
+                    if (sbtn != document.activeElement) {
+                        if (e.keyCode === keyArray[i]) {//按下对应按键并且搜索框没有焦点
+                            keytype[i].click();//点击对应i的链接
+                        }
+                    }
+                }
+                if (sbtn == document.activeElement) {//搜索框有焦点
+                    if (e.keyCode == 27) {
+                        sbtn.blur();//去焦点
+                    }
+                    if (e.keyCode == 13) {//enter进行搜索
+                        //enter不会让input丢失聚焦，所以要先取值，因为不能执行到remove里的取值
+                        content = sbtn.value;
+                        searchMy();
+                    };
+                } else {
+                    if (e.keyCode == 27) {
+                        sbtn.focus();//没有焦点时获得焦点
+                    }
+                }
             }
         }
     }
